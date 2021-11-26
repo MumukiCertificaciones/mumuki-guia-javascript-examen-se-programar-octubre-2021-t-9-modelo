df06fdd0ee24b2038b65d508025725b2dcbf0caa
gustavo@mumuki.org <gustavo@mumuki.org>
2021-11-26 17:13:40 +0000

    Mumuki Export on 2021-11-26 17:13:40 +0000

--- 00004_Ejercicio 4/test..js
+++ 00004_Ejercicio 4/test..js
@@ -4,18 +4,18 @@ function transform(texto) {
 
 describe("", function() {
   it("Saludar a Lu a las 11 retorna 'Buenos días Lu'", function() {
-    assert.equal(transform(saludar("Lu", 11), transform("buenos días lu"));
+    assert.equal(transform(saludar("Lu", 11)), "buenos dias lu");
   });
   
   it("Saludar a Ale a las 10 retorna 'Buenos días Ale'", function() {
-    assert.equal(transform(saludar("Ale", 10)), transform("buenos días ale"));
+    assert.equal(transform(saludar("Ale", 10)), "buenos dias ale");
   });
   
   it("Saludar a Agus a las 12 retorna 'Buenas tardes Agus'", function() {
-    assert.equal(transform(saludar("Agus",12), transform("buenas tardes agus"));
+    assert.equal(transform(saludar("Agus",12)), "buenas tardes agus");
   });
   
   it("Saludar a Guille a las 14 retorna 'Buenas tardes Guille'", function() {
-    assert.equal(transform(saludar("Guille", 14), transform("buenas tardes guille"));
+    assert.equal(transform(saludar("Guille", 14)), "buenas tardes guille");
   });
 })
