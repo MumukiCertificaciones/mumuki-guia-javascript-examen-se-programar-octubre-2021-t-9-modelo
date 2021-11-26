b2106cd6b81455646ca4c6815f8b4f740e1c7254
mayra@mumuki.org <mayra@mumuki.org>
2021-11-26 16:15:23 +0000

    Mumuki Export on 2021-11-26 16:15:23 +0000

--- 00003_Ejercicio 3/description.md
+++ 00003_Ejercicio 3/description.md
@@ -1,11 +1,11 @@
-Ahora vamos a crear una función `$FUNCION` que recibe tres strings y nos dice si la _concatenación_ de los dos primeros es igual al tercero :thinking::
+Ahora vamos a definir una función `concatenacionEsIgual` que recibe tres strings y nos dice si la _concatenación_ de los dos primeros es igual al tercero :thinking::
 
 ``` javascript
-ム $FUNCION("mari", "posa", "langosta")
+ム concatenacionEsIgual("mari", "posa", "langosta")
 false
 
-ム $FUNCION("guarda", "ropas", "guardaropas")
+ム concatenacionEsIgual("guarda", "ropas", "guardaropas")
 true
 ```
 
-> Definí la función `$FUNCION`.
+> Definí la función `concatenacionEsIgual`.
--- 00003_Ejercicio 3/test..js
+++ 00003_Ejercicio 3/test..js
@@ -1,17 +1,17 @@
 describe("", function() {
   it("la concatenacion de 'mari' y 'posa' es igual a 'mariposa'", function() {
-    assert($FUNCION("mari", "posa", "mariposa"));
+    assert(concatenacionEsIgual("mari", "posa", "mariposa"));
   });
   
   it("la concatenacion de 'guarda' y 'polvos' es igual a 'guardapolvos'", function() {
-    assert($FUNCION("guarda", "polvos", "guardapolvos"));
+    assert(concatenacionEsIgual("guarda", "polvos", "guardapolvos"));
   });
   
   it("la concatenacion de 'yerba' y 'mate' no es igual a 'cafe'", function() {
-    assert(!$FUNCION("yerba", "mate", "cafe"));
+    assert(!concatenacionEsIgual("yerba", "mate", "cafe"));
   });
   
   it("la concatenacion de 'murcie' y 'lago' no es igual a 'linterna'", function() {
-    assert(!$FUNCION("murcie", "lago", "linterna"));
+    assert(!concatenacionEsIgual("murcie", "lago", "linterna"));
   });
 })
