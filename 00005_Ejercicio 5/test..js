dfa1b2bda15626e4f5af0e1cdee73eebd16c0433
mayra@mumuki.org <mayra@mumuki.org>
2021-11-26 15:37:17 +0000

    Mumuki Export on 2021-11-26 15:37:17 +0000

--- 00005_Ejercicio 5/test..js
+++ 00005_Ejercicio 5/test..js
@@ -1,13 +1,13 @@
 describe("", function() {
-  it("la $FUNCION de la lista con 2, 5, 9 y 8 es 17", function() {
-    assert.equal($FUNCION([2, 5, 9, 8]), 17);
+  it("la sumaDeLosMayoresASiete de la lista con 2, 5, 9 y 8 es 17", function() {
+    assert.equal(sumaDeLosMayoresASiete([2, 5, 9, 8]), 17);
   });
   
-  it("la $FUNCION de la lista con 3, 3, 8 es 8", function() {
-    assert.equal($FUNCION([3, 3, 8]), 8);
+  it("la sumaDeLosMayoresASiete de la lista con 3, 3, 8 es 8", function() {
+    assert.equal(sumaDeLosMayoresASiete([3, 3, 8]), 8);
   });
   
-  it("la $FUNCION de la lista con 3, 5, 6, 1, 7 es 0", function() {
-    assert.equal($FUNCION([3, 5, 6, 1, 7]), 0);
+  it("la sumaDeLosMayoresASiete de la lista con 3, 5, 6, 1, 7 es 0", function() {
+    assert.equal(sumaDeLosMayoresASiete([3, 5, 6, 1, 7]), 0);
   });
 })
