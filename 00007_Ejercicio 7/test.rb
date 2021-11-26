9a54b9bddae9822e5a44aadc742882dc5427e972
mayra@mumuki.org <mayra@mumuki.org>
2021-11-26 15:57:13 +0000

    Mumuki Export on 2021-11-26 15:57:13 +0000

--- 00007_Ejercicio 7/test.rb
+++ 00007_Ejercicio 7/test.rb
@@ -15,17 +15,17 @@ it "Una instancia de AguaParaMate agrega 10 grados de temperatura al enviarle ca
   expect(aguaParaMate.temperatura).to be 75
 end
 
-it "Una instancia de AguaParaMate está en la temperatura correcta si está a $TEMPERATURA_CORRECTA grados" do
+it "Una instancia de AguaParaMate está en la temperatura correcta si está a 80 grados" do
   aguaParaMate = AguaParaMate.new($TEMPERATURA_CORRECTA)
-  expect(aguaParaMate.$METODO_VERIFICAR).to be true
+  expect(aguaParaMate.temperatura_exacta?).to be true
 end
 
-it "Una instancia de AguaParaMate no está en la temperatura correcta si está a menos de $TEMPERATURA_CORRECTA grados" do
-  aguaParaMate = AguaParaMate.new($TEMPERATURA_CORRECTA - 1)
-  expect(aguaParaMate.$METODO_VERIFICAR).to be false
+it "Una instancia de AguaParaMate no está en la temperatura correcta si está a menos de 80 grados" do
+  aguaParaMate = AguaParaMate.new(79)
+  expect(aguaParaMate.temperatura_exacta?).to be false
 end
 
-it "Una instancia de AguaParaMate no está en la temperatura correcta si está a más de $TEMPERATURA_CORRECTA grados" do
-  aguaParaMate = AguaParaMate.new($TEMPERATURA_CORRECTA + 1)
-  expect(aguaParaMate.$METODO_VERIFICAR).to be false
+it "Una instancia de AguaParaMate no está en la temperatura correcta si está a más de 80 grados" do
+  aguaParaMate = AguaParaMate.new(81)
+  expect(aguaParaMate.temperatura_exacta?).to be false
 end
